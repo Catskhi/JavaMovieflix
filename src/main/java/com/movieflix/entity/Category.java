41cf5df0d76e9edd1dc755395b934632ec268821
catskhi <catskhi@proton.me>
2025-02-03 22:23:04 -0300

    feat: Added Category controller, entity, repository, service and mapper. Added category request and response records too.

---
+++ src/main/java/com/movieflix/entity/Category.java
@@ -0,0 +1,23 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@Entity
+@Table(name = "category")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+}
