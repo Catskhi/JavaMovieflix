1b0d38b2f13ff775e6cbf25533b3678f16562646
catskhi <catskhi@proton.me>
2025-02-04 22:06:44 -0300

    feat: Added Movie entity and repository

---
+++ src/main/java/com/movieflix/entity/Movie.java
@@ -0,0 +1,42 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    private double rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
