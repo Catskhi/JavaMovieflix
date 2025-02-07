7d6ae7e6810456dfa22578ee3f0c1bd7ab379741
catskhi <catskhi@proton.me>
2025-02-06 23:57:34 -0300

    refactor: Added findCategories and findStreamings methods, added categories and streamigs to the save movie method

--- src/main/java/com/movieflix/service/MovieService.java
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -1,23 +1,48 @@
 package com.movieflix.service;
 
+import com.movieflix.entity.Category;
 import com.movieflix.entity.Movie;
+import com.movieflix.entity.Streaming;
 import com.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(findCategories(movie.getCategories()));
+        movie.setStreamings(findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
     }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> {
+            categoryService.findById(category.getId()).ifPresent(categoriesFound::add);
+        });
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> {
+            streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add);
+        });
+        return streamingsFound;
+    }
+
 }
