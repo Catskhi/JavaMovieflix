a0ae91c625c3f61e98a57d5323691db4eda20d5a
catskhi <catskhi@proton.me>
2025-02-07 22:43:50 -0300

    feat: Added User model, mapper, repository and service

---
+++ src/main/java/com/movieflix/entity/User.java
@@ -0,0 +1,30 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
