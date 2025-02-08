121bedc1a2ab32f30b78adfd7974cf8259ac3f1b
catskhi <catskhi@proton.me>
2025-02-07 21:59:20 -0300

    feat: Added findMovieByCategories and findTop5ByOrderByRatingDesc methods to MovieRepository interface

--- src/main/java/com/movieflix/repository/MovieRepository.java
+++ src/main/java/com/movieflix/repository/MovieRepository.java
@@ -1,7 +1,14 @@
 package com.movieflix.repository;
 
+import com.movieflix.entity.Category;
 import com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
+
+    List<Movie> findTop5ByOrderByRatingDesc();
 }
