27ec5ae81cd58b9b4d2f58f8dc23c794ddebf096
catskhi <catskhi@proton.me>
2025-02-11 17:51:49 -0300

    feat: Added swagger documentation to routes

--- src/main/java/com/movieflix/controller/MovieController.java
+++ src/main/java/com/movieflix/controller/MovieController.java
@@ -5,6 +5,12 @@ import com.movieflix.controller.response.MovieResponse;
 import com.movieflix.entity.Movie;
 import com.movieflix.mapper.MovieMapper;
 import com.movieflix.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +20,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/movieflix/movie")
 @RequiredArgsConstructor
+@Tag(name = "Movie", description = "Resource responsible for the movies management.")
 public class MovieController {
     private final MovieService movieService;
 
+    @Operation(summary = "Save movie", description = "Method responsible of saving movies",
+            security = @SecurityRequirement(name = "BearerAuth"))
+    @ApiResponse(responseCode = "201", description = "Movie successfully saved.",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @PostMapping
     public ResponseEntity<MovieResponse> save(@Valid @RequestBody MovieRequest request) {
         Movie savedMovie = movieService.save(MovieMapper.toMovie(request));
         return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
     }
 
+    @Operation(summary = "List movies", description = "List all registered movies.",
+            security = @SecurityRequirement(name = "BearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Return all registered movies.",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @GetMapping
     public ResponseEntity<List<MovieResponse>> findAll() {
         return ResponseEntity.ok(movieService.findAll()
@@ -34,6 +49,11 @@ public class MovieController {
                 .toList());
     }
 
+    @Operation(summary = "Find movie by id", description = "List a movie by id.",
+            security = @SecurityRequirement(name = "BearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Movie successfully found.",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Movie not found.")
     @GetMapping("/{id}")
     public ResponseEntity<MovieResponse> findById(@PathVariable Long id) {
         return movieService.findById(id)
@@ -41,6 +61,11 @@ public class MovieController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Update movie", description = "Update movie by id",
+            security = @SecurityRequirement(name = "BearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Movie successfully updated.",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Movie not found.")
     @PutMapping("/{id}")
     public ResponseEntity<MovieResponse> update(@PathVariable Long id,@Valid @RequestBody MovieRequest request) {
         return movieService.update(MovieMapper.toMovie(request), id)
@@ -48,6 +73,11 @@ public class MovieController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Find movies by category", description = "Find movies by category.",
+            security = @SecurityRequirement(name = "BearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Movies successfully found.",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Movies not found.")
     @GetMapping("/search")
     public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam("category") Long categoryId) {
         return ResponseEntity.ok(movieService.findByCategory(categoryId).stream()
@@ -55,6 +85,11 @@ public class MovieController {
                 .toList());
     }
 
+    @Operation(summary = "Delete movie by id", description = "Delete movies by id.",
+            security = @SecurityRequirement(name = "BearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Movies successfully deleted.",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Movie not found.")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         Optional<Movie> existentMovie = movieService.findById(id);
