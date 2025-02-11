dd9c55274da396d9042030027bf8b5e919033761
catskhi <catskhi@proton.me>
2025-02-11 17:52:01 -0300

    feat: Added swagger documentation to records

--- src/main/java/com/movieflix/controller/request/MovieRequest.java
+++ src/main/java/com/movieflix/controller/request/MovieRequest.java
@@ -1,18 +1,24 @@
 package com.movieflix.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record MovieRequest(
-        @NotEmpty(message = "Movie title is required.") String title,
-        String description,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-        LocalDate releaseDate,
-        double rating,
-        List<Long> categories,
-        List<Long> streamings
+public record MovieRequest(@Schema(type = "String", description = "Movie title")
+                @NotEmpty(message = "Movie title is required.") String title,
+                @Schema(type = "String", description = "Movie description")
+                String description,
+                @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+                @Schema(type = "date", description = "Release date of movie. e.g: 19/01/2001")
+                LocalDate releaseDate,
+                @Schema(type = "double", description = "Movie score. e.g: 7.8")
+                double rating,
+                @Schema(type = "array", description = "List of categories ids.")
+                List<Long> categories,
+                @Schema(type = "array", description = "List of streamings ids.")
+                List<Long> streamings
 ) {
 }
--- src/main/java/com/movieflix/controller/response/MovieResponse.java
+++ src/main/java/com/movieflix/controller/response/MovieResponse.java
@@ -1,20 +1,27 @@
 package com.movieflix.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieResponse(
+public record MovieResponse(@Schema(type = "long", description = "Movie id")
         Long id,
+        @Schema(type = "String", description = "Movie title")
         String title,
+        @Schema(type = "String", description = "Movie description")
         String description,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+        @Schema(type = "Date", description = "Release date of movie. e.g: 19/01/2001")
         LocalDate releaseDate,
+        @Schema(type = "double", description = "Movie score. e.g: 7.8")
         double rating,
+        @Schema(type = "array", description = "List of categories ids.")
         List<CategoryResponse> categories,
+        @Schema(type = "array", description = "List of streamings ids.")
         List<StreamingResponse> streamings
 ) {
 }
